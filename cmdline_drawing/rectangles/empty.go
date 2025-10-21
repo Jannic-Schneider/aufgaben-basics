07374f07ee93a185d0a4b75156f4686dd3658ab0
Jannic Schneider <Jannschn22@gmail.com>
2025-10-21 15:07:38 +0000

    Commit

--- cmdline_drawing/rectangles/empty.go
+++ cmdline_drawing/rectangles/empty.go
@@ -1,12 +1,22 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Der Rand des Rechtecks soll aus `#`-Zeichen bestehen, der Innenraum soll leer sein.
 func DrawEmptyRectangle(height, width int) {
-	// TODO
-}
 
-// REMARKS
-// - Diese Aufgabe ist eine etwas komplexere Variante der Aufgabe "Gefülltes Rechteck zeichnen".
-//   Man geht dabei ähnlich vor, muss allerdings zusätzlich prüfen, ob man sich am Rand des Rechtecks befindet.
+	for row := 0; row < height; row++ {
+
+		for col := 0; col < width; col++ {
+			if row == 0 || col == 0 || row == height-1 || col == width-1 {
+
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
